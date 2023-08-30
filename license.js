6ac6cf2d5aabf373f0d9afb8d5a02e8960166447
Nikita Turbo <117271258+NikitaTurbo@users.noreply.github.com>
2023-08-30 13:54:10 +0300

    Update license.js

--- license.js
+++ license.js
@@ -1,7 +1,7 @@
 const license = document.querySelector('input');
 const licenseLabel = document.querySelector('label');
 const succes = document.querySelector('div div');
-const licensesName = ['4fBa0oAcd883zW-129', '83xVwOA0fd127-05', 'A012fdcWx135x-202'];
+const licensesName = ['4fBa0oAcd883zW-129', '83xVwOA0fd127-05', 'A012fdcWx135x-202', 'b0fhgT57-45tBA-202c'];
 
 const search = (value) => {
     if (licensesName.indexOf(value) >= 0) {
