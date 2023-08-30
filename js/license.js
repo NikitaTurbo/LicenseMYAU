26ed8f1cfbe73d13cb7d72a79d3ff823562b3c11
Nikita Turbo <117271258+NikitaTurbo@users.noreply.github.com>
2023-08-30 11:22:12 +0300

    MYAU

---
+++ js/license.js
@@ -0,0 +1,12 @@
+const license = document.querySelector('input');
+const licenseLabel = document.querySelector('label');
+const succes = document.querySelector('div div');
+const licensesName = ['4fBa0oAcd883zW-129', '83xVwOA0fd127-05', 'A012fdcWx135x-202'];
+
+const search = (value) => {
+    if (licensesName.indexOf(value) >= 0) {
+        license.style.display = 'none';
+        licenseLabel.style.display = 'none';
+        succes.style.display = 'block';
+    }
+}
